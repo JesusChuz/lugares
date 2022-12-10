4e82165da08c71520b61a2d30c0491aa171b42fa
JesusChuz <j.mendezquesada18@gmail.com>
2022-12-09 22:58:48 -0600

    Primer commit: Registro + Login en Firebase

---
+++ app/src/main/java/com/example/lugares/ui/lugar/LugarFragment.kt
@@ -0,0 +1,52 @@
+package com.example.lugares.ui.lugar
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.lugares.databinding.FragmentLugaresBinding
+import com.example.lugares.viewmodel.LugarViewModel
+
+class LugarFragment : Fragment() {
+    private lateinit var lugarViewModel: LugarViewModel
+    private var _binding: FragmentLugaresBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        lugarViewModel = ViewModelProvider(this).get(LugarViewModel::class.java)
+        _binding = FragmentLugaresBinding.inflate(inflater, container, false)
+
+       /* binding.btAddLugar.setOnClickListener {
+            findNavController().navigate(R.id.action_nav_lugar_to_addLugarFragment)
+        } */
+
+        //Listado lugares | activar el recyclerview
+        /*(val lugarAdapter = LugarAdapter()
+        val reciclador = binding.reciclador
+        reciclador.adapter = lugarAdapter
+        reciclador.layoutManager = LinearLayoutManager(requireContext())
+
+        lugarViewModel = ViewModelProvider(this)[LugarViewModel::class.java]
+
+        lugarViewModel.obtenerLugares.observe(viewLifecycleOwner){
+                lugares -> lugarAdapter.setLugares(lugares)
+            //lugarAdapter.setLugars(it)
+        }*/
+
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/com/example/lugares/viewmodel/LugarViewModel.kt
@@ -0,0 +1,30 @@
+package com.example.lugares.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+/*import com.example.lugares.data.LugarDatabase
+import com.example.lugares.model.Lugar
+import com.example.lugares.repository.LugarRepository*/
+import kotlinx.coroutines.launch
+
+class LugarViewModel(application: Application) : AndroidViewModel(application) {
+    /*val obtenerLugares: LiveData<List<Lugar>>
+    private val repository: LugarRepository
+
+    init {
+        val lugarDao = LugarDatabase.getDatabase(application).lugarDao()
+        repository = LugarRepository(lugarDao)
+        obtenerLugares = repository.obtenerLugares
+    }
+    fun guardarLugar(lugar: Lugar){
+        viewModelScope.launch { repository.guardarLugar(lugar) }
+    }
+    fun updateLugar(lugar: Lugar){
+        viewModelScope.launch { repository.updateLugar(lugar) }
+    }
+    fun eliminarLugar(lugar: Lugar){
+        viewModelScope.launch { repository.eliminarLugar(lugar) }
+    }*/
+}
